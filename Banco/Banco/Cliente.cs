88ca11b60da97736aad956aef5555979b7367f39
Gabriel Miranda <46597832+Gaba-tec@users.noreply.github.com>
2023-09-07 15:44:20 -0300

    Windows Form Estudo

---
+++ Banco/Banco/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace Banco
+{
+	public class Cliente
+	{
+		public string Nome { get; set; }
+
+		public Cliente()
+		{
+		}
+
+		public Cliente(string v)
+		{
+			this.Nome = v;
+		}
+	}
+}
---
+++ Banco/Banco/Conta.cs
@@ -0,0 +1,16 @@
+﻿namespace Banco
+{
+	public class Conta
+	{
+
+		public int Numero { get; set; }
+
+		public double Saldo { get; private set; }
+		public Cliente Titular { get; internal set; }
+
+		public Conta()
+		{
+
+		}
+	}
+}
