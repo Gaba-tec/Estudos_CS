3438ea7cffb33b0a9f935d5ec2cd5c9414ea7af1
Gabriel Miranda <gabrielmirandazacarias@gmail.com>
2023-10-23 11:10:22 -0300

    Estudos Api ViaCep

--- Tech/CepApi/Program.cs
+++ Tech/CepApi/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CepApi;
+using Refit;
+
+class Program
+{
+	static async Task Main(string[] args)
+	{
+		try
+		{
+			var cepClient = RestService.For<ICepApiService>("http://viacep.com.br/ws");
+			Console.WriteLine("Informe seu cep:");
+
+			string cepInformado = Console.ReadLine().ToString();
+			Console.WriteLine("Consultando informações do cep: " + cepInformado);
+
+			var address = await cepClient.GetAddressAsync(cepInformado);
+			Console.WriteLine($"\nLogradouro:{address.Logradouro},\nBairro:{address.Bairro},\nCidade{address.Localidade}");
+			Console.ReadKey();
+
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Erro na consulta de cep: " + ex.Message);
+		}
+	}
+}
+
+
+
