d5921b77097c12620efd983f02ed70ac04f5e2d2
Gabriel Miranda <gabrielmirandazacarias@gmail.com>
2023-10-30 20:57:28 -0300

    Aprendendo NuGet

---
+++ Tech/GeneralValidation/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace GeneralValidation
+{
+	public static class Class1
+	{
+		public static string ValidaCpf(string cpf)
+		{
+			if(cpf != null)
+			{
+				return cpf;
+			}
+			else
+			{
+				return "Cpf invalido";
+			}
+		}
+	}
+}
