7d1728ec65821349d51d115ef706ae8b62aa3b61
Gaba-tec <gabrielmirandazacarias@gmail.com>
2023-11-06 06:02:58 -0300

    Estudando System.IO

---
+++ csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,50 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+
+            var endereco = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(endereco, FileMode.Open);
+
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024];
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+				EscreveBuffer(buffer);
+			}
+
+
+            Console.ReadLine();
+        }
+
+        static void EscreveBuffer(byte[] buffer)
+        {
+
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+
+            //Console.Write(texto);
+
+            foreach (var meuByte in texto)
+            {
+                Console.Write(meuByte);
+            }
+        }
+    }
+} 
+ 
--- curso_bibliotecas-Aula02/bytebank_ATENDIMENTO/Program.cs
+++ curso_bibliotecas-Aula02/bytebank_ATENDIMENTO/Program.cs
@@ -3,7 +3,7 @@ using bytebank_ATENDIMENTO.bytebank.Atendimento;
 using bytebank_GeradorChavePix;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-//new ByteBankAtendimento().AtendimentoCliente();
+new ByteBankAtendimento().AtendimentoCliente();
 
 Console.WriteLine(GeradorPix.GetChavePix());
 
